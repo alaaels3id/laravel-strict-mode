eb71557f02a32a5e0af8e4096f7241e36c950a9b
Alaa Elsaid <alaa4cat@gmail.com>
2024-01-25 17:43:15 +0200

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+//        Model::preventLazyLoading(!app()->isProduction());
+//
+//        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+//
+//        Model::preventAccessingMissingAttributes(!app()->isProduction());
+
+        Model::shouldBeStrict(!app()->isProduction());
+
+//        Model::unguard();
+    }
+}
